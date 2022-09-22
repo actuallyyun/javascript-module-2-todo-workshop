10abe294c3b5fcb47ae48d62e16379d1814e0bd7
Yun Ji <this.jiyun@gmail.com>
2022-09-22 11:02:39 +0200

    finished all homework

--- scripts/script.js
+++ scripts/script.js
@@ -1,44 +1,70 @@
-document.querySelector('#new-todo').addEventListener('submit', (e) => {
-    e.preventDefault()
-    const input = e.target.elements.text.value.trim()
-    if (input.length > 0) {
-
-
-        createTodo(input)
-        e.target.elements.text.value = ''
-    }
-    renderTodos(todos)
-})
-
-
 const todos = []
 
+const filters = {
+    searchTitle: '',
+    showFinished: false,
+    showUnfinished: false,
+}
+
+
+
+const setFilters = (updates) => {
+    if (typeof (updates.searchTitle) === 'string') {
+        filters.searchTitle = updates.searchTitle
+    }
+    if (typeof (updates.showFinished) === 'boolean') {
+        filters.showFinished = updates.showFinished
+    }
+    if (typeof (updates.showUnfinished) === 'boolean') {
+        filters.showUnfinished = updates.showUnfinished
+    }
+}
+
+
+
 const createTodo = (input) => {
-    todos.push(input)
+    todos.push({
+        title: input,
+        completed: false
+    })
+
 }
 
 //ex 6
-const generateTodoDOM = (todo) => {
+const generateTodoDOM = (todoObj) => {
+
+    //set up checkbox
+    const checkbox = document.createElement('input')
+    checkbox.setAttribute('type', 'checkbox')
+    checkbox['checked'] = todoObj.completed
+
     const todoEl = document.createElement('label')
     const containerEl = document.createElement('div')
     const todoText = document.createElement('span')
 
 
-    todoText.textContent = todo
+    todoText.textContent = todoObj.title
     containerEl.appendChild(todoText)
+
+    containerEl.appendChild(checkbox)
 
     todoEl.classList.add('list-item')
     containerEl.classList.add('list-item_container')
 
     todoEl.appendChild(containerEl)
 
+    //eventListener to checkbox
+    checkbox.addEventListener('change', (e) => {
+        toggleTodo(todoObj.title)
+        renderTodos(todos)
+    })
     //remove button
     const removeBtn = document.createElement('button')
     removeBtn.textContent = 'remove'
     removeBtn.classList.add('button', 'button--text')
     todoEl.appendChild(removeBtn)
     removeBtn.addEventListener('click', () => {
-        removeTodo(todoText)
+        removeTodo(todoObj.title)
         renderTodos(todos)
     })
 
@@ -66,9 +92,10 @@ const renderTodos = (todos) => {
 renderTodos(todos)
 
 //ex 9
-const removeTodo = (todoEl) => {
+const removeTodo = (title) => {
+
     const todoIndex = todos.findIndex((todo) => {
-        return todo.toLowerCase() === todoEl.textContent.toLowerCase()
+        return todo.title.toLowerCase() === title.toLowerCase()
     })
     if (todoIndex > -1) {
         todos.splice(todoIndex, 1)
@@ -76,3 +103,47 @@ const removeTodo = (todoEl) => {
 }
 
 
+const toggleTodo = (title) => {
+    const todo = todos.find((todo) => todo.title.toLowerCase() === title.toLowerCase())
+    if (todo) {
+        todo.completed = !todo.completed
+    }
+}
+
+
+document.querySelector('#new-todo').addEventListener('submit', (e) => {
+    e.preventDefault()
+    const input = e.target.elements.text.value.trim()
+    if (input.length > 0) {
+
+
+        createTodo(input)
+        e.target.elements.text.value = ''
+    }
+    renderTodos(todos)
+})
+
+//add input event to input element
+document.querySelector('#search-text').addEventListener('input', (e) => {
+
+    setFilters({
+        searchTitle: e.target.value,
+    })
+    renderTodos(todos)
+})
+
+//add change event to finished checkbox
+document.querySelector('#show-finished').addEventListener('change', (e) => {
+    setFilters({
+        showFinished: e.target.checked
+    })
+    renderTodos(todos)
+})
+
+//add change event to unfinished checkbox
+document.querySelector('#show-unfinished').addEventListener('change', (e) => {
+    setFilters({
+        showUnfinished: e.target.checked
+    })
+    renderTodos(todos)
+})
